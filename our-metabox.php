9401551cd9b02af5f4db46fc5cc10473915d4467
Sohan Chowdhury <89298094+prosohan7@users.noreply.github.com>
2022-06-27 10:29:07 +0600

    Update our-metabox.php

--- our-metabox.php
+++ our-metabox.php
@@ -18,14 +18,49 @@
         add_action( 'plugins_loaded', array( $this, 'omb_load_textdomain' ) );
         // add_action( 'plugins_loaded', 'pqrc_load_textdomain');
         add_action( 'admin_menu', array( $this, 'omb_add_metabox' ) );
+        add_action( 'save_post', array( $this, 'omb_save_location' ) );
     }
 
-    function omb_save_location($post_id){
-        $location = isset($_POST['omb_location']) ? $_POST['omb_location'] : '';
-        if ( $location == '') {
+    private function is_secured( $nonce_field, $action, $post_id ) {
+        $nonce = isset($_POST[$nonce_field]) ? $_POST[$nonce_field] : '';
+        
+        if ( $nonce == '' ) {
+            return false;
+        }
+        if ( !wp_verify_nonce( $nonce, $action ) ) {
+            return false;
+        }
+        if ( !current_user_can( 'edit_post', $post_id ) ) {
+            return false;
+        }
+        if ( wp_is_post_autosave($post_id) ) {
+            return false;
+        } 
+        if ( wp_is_post_revision($post_id) ) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    function omb_save_location( $post_id ){
+
+        if ( !$this->is_secured( 'omb_location_field', 'omb_location', $post_id ) ) {
             return $post_id;
         }
+
+        $location = isset($_POST['omb_location']) ? $_POST['omb_location'] : '';
+        $country = isset($_POST['omb_country']) ? $_POST['omb_country'] : '';
+
+        if ( $location == '' || $country == '') {
+            return $post_id;
+        }
+        $location = sanitize_text_field( $location );
+        $country = sanitize_text_field( $country );
+        
         update_post_meta( $post_id, 'omb_location', $location );
+        update_post_meta( $post_id, 'omb_country', $country );
         // add_post_meta( $post_id:integer, $meta_key:string, $meta_value:mixed, $unique:boolean )
     }  
 
@@ -40,13 +75,21 @@
 
     function omb_display_post_location($post) {
         $location = get_post_meta($post->ID, 'omb_location', true);
+        $country = get_post_meta($post->ID, 'omb_country', true);
         //get_post_meta( $post_id:integer, $key:string, $single:boolean )
-        $label = __( 'Location ', 'our-metabox' );
+        $label1 = __( 'Location ', 'our-metabox' );
+        $label2 = __( 'Country ', 'our-metabox' );
+        wp_nonce_field( 'omb_location', 'omb_location_field' );
+        // wp_nonce_field( 'omb_country', 'omb_location_field' );
         $metabox_html = <<<EOD
-            <p>
-                <label for="omb_location">{$label}</label>
-                <input type="text" name="omb_location" id="omb_location" value="{$location}" />
-            </p>
+        <p>
+            <label for="omb_location">{$label1}</label>
+            <input type="text" name="omb_location" id="omb_location" value="{$location}" />
+        </p>
+        <p>
+            <label for="omb_country">{$label2}</label>
+            <input type="text" name="omb_country" id="omb_country" value="{$country}" />
+        </p>
         EOD;
 
         echo $metabox_html;
